be70a695244376f72d3f0a0c16e64f812de673a1
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-10-28 12:33:25 +0600

    UITabBarController added

---
+++ FileManager/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  FileManager
+//
+//  Created by Konstantin Bolgar-Danchenko on 28.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Settings"
+        view.backgroundColor = .white
+    }
+}
